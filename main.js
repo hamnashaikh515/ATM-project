e45b1f974134d83a1ad48405e80e9a01e223f31d
Hamna Shaikh <160252803+hamnashaikh515@users.noreply.github.com>
2024-05-30 08:29:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+let myBalance = 10000; //Dollars
+let myPin = 4321;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+        if (amountAns.amount <= myBalance) {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining balance is  ${myBalance}`);
+        }
+        else {
+            console.log("Insufficient balance");
+        }
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("Your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect pin code!!!");
+}
